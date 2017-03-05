93af33c6f6fc97313647948d94e18c345db55ad3
Mike Prime <michelmbili@gmail.com>
2017-03-05 01:38:20 +0100

    Init connection to the database

---
+++ app/vendor/_database.php
@@ -0,0 +1,21 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', '[DB_NAME]');
+define('DB_USERNAME', '[DB_USERNAME]');
+define('DB_PASSWORD', '[DB_PASSWORD]');
+
+try {
+    
+    $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+
+    return $db;
+
+} catch (PDOException $e) {
+    
+    die('Erreur: '.$e->getMessage());
+
+}
