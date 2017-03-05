c59f7ace5714191eb5ca6075d0016ad47e8791a6
Mike Prime <michelmbili@gmail.com>
2017-03-05 01:16:24 +0100

    Basic styles for the home page

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,7 @@
-<h1>Prime</h1>
-
-<p>
-	Prime is PHP Framework base on MVC design pattern and adapted for Procedural PHP programming
-</p>
+<div class="wrapper">
+	<h1>Prime.</h1>
+	
+	<p>
+		Prime is PHP Framework base on MVC design pattern and adapted for Procedural PHP programming.
+	</p>
+</div>
--- pages/templates/default.php
+++ pages/templates/default.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title><?= $title; ?> | <?= projet_name(); ?></title>
+	<link rel="stylesheet" href="public/css/app.css">
 </head>
 <body>
 
